b68db149e9b489bc5cd7767f59a8d10f19c6ef83
Siddhesh Mangela <sidthecool007@gmail.com>
2022-03-30 15:30:47 +0530

    Dud manula chunk name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#outputmanualchunks
+      output: {
+        manualChunks: {
+          dud: ['./src/components/Dud'],
+        },
+      },
+    },
+  },
+});
